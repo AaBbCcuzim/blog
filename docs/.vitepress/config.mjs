450f0f0ba48c7f8dca0afc61d8fe0d5aca7b223f
AaBbCcuzim <1848028203@qq.com>
2024-03-30 15:59:54 +0800

    1

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Yikes Xavier",
+  base: "/blog/",
+  description: "Yikes Xavier的全栈技术补给站",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Yikes Xavier"
+  text: "的全栈技术补给站"
+  tagline: 努力发掘最新的全栈技术
+  image:
+
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /markdown-examples
+    - theme: alt
+      text: 文档中心
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
